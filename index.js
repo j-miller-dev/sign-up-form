1b6255b562c58477bb71c58e9a843968b4b9ac3f
Jason Miller <jasonmiller.dev@gmail.com>
2022-09-13 13:26:17 +1000

    Passwords match js, focus border change, need to fix on submit

--- index.js
+++ index.js
@@ -8,99 +8,86 @@ submitButton.addEventListener("click", function () {
 });
 submitButton.addEventListener("click", validateForm);
 
+const pw1 = document.getElementById("pswd1");
+const pw2 = document.getElementById("pswd2");
+
 // Language: javascript
 function validateForm() {
-  let pw1 = document.getElementById("pswd1");
-  let pw2 = document.getElementById("pswd2");
-  let name1 = document.getElementById("name1");
-  let name2 = document.getElementById("name2");
-
-  //check empty first name field
-  if (name1 == "") {
-    document.getElementById("blankMsg").innerHTML = "**Fill the first name";
-    return false;
-  }
-
-  //character data validation
-  if (!isNaN(name1)) {
-    document.getElementById("blankMsg").innerHTML =
-      "**Only characters are allowed";
-    return false;
-  }
-
-  //character data validation
-  if (!isNaN(name2)) {
-    document.getElementById("charMsg").innerHTML =
-      "**Only characters are allowed";
-    return false;
-  }
-
-  //check empty password field
-  if (pw1 == "") {
-    document.getElementById("message1").innerHTML =
-      "**Fill the password please!";
-    return false;
-  }
-
-  //check empty confirm password field
-  if (pw2 == "") {
-    document.getElementById("message2").innerHTML =
-      "**Enter the password please!";
-    return false;
-  }
-
-  //minimum password length validation
-  if (pw1.length < 8) {
-    document.getElementById("message1").innerHTML =
-      "**Password length must be atleast 8 characters";
-    return false;
-  }
-
-  //maximum length of password validation
-  if (pw1.length > 15) {
-    document.getElementById("message1").innerHTML =
-      "**Password length must not exceed 15 characters";
-    return false;
-  }
-
-  if (pw1 === pw2) {
-    console.log("success");
-    alert("Your password created successfully");
-    document.write("JavaScript form has been submitted successfully");
+  console.log("validate form");
+  if (pw1.value !== pw2.value) {
+    document.getElementById("message2").innerHTML = "Passwords are not same";
   } else {
-    console.log("passwords are not same");
-    // document.getElementById("message2").innerHTML = "**Passwords are not same";
-    return false;
+    alert("Passwords match");
   }
 }
 
-function nameValidate() {
-  console.log("Name validation");
-  let name1 = document.getElementById("name1");
-  let name2 = document.getElementById("name2");
+// function validateForm() {
+//   console.log("validate form");
+//   //check empty password field
+//   if (pw1 == "") {
+//     document.getElementById("message1").innerHTML =
+//       "**Fill the password please!";
+//     return false;
+//   }
 
-  //check empty first name field
-  if (name1 == "") {
-    document.getElementById("blankMsg").innerHTML = "**Fill the first name";
-    return false;
-  }
-  if (name2 == "") {
-    document.getElementById("blankMsg").innerHTML = "**Fill the last name";
-    return false;
-  }
+//   //check empty confirm password field
+//   if (pw2 == "") {
+//     document.getElementById("message2").innerHTML =
+//       "**Enter the password please!";
+//     return false;
+//   }
 
-  //character data validation
-  if (!isNaN(name1)) {
-    console.log("only characters are allowed");
-    document.getElementById("charMsg").innerHTML =
-      "**Only characters are allowed";
-    return false;
-  }
-  if (!isNaN(name2)) {
-    document.getElementById("charMsg").innerHTML =
-      "**Only characters are allowed";
-    return false;
-  }
-}
+//   //minimum password length validation
+//   if (pw1.length < 8) {
+//     document.getElementById("message1").innerHTML =
+//       "**Password length must be atleast 8 characters";
+//     return false;
+//   }
+
+//   //maximum length of password validation
+//   if (pw1.length > 15) {
+//     console.log("too long");
+//     document.getElementById("message1").innerHTML =
+//       "**Password length must not exceed 15 characters";
+//     return false;
+//   }
+
+//   if (pw1 === pw2) {
+//     console.log("success");
+//     alert("Your password created successfully");
+//     document.write("JavaScript form has been submitted successfully");
+//   } else {
+//     console.log("passwords are not same");
+//     // document.getElementById("message2").innerHTML = "**Passwords are not same";
+//     return false;
+//   }
+// }
+
+// function nameValidate() {
+//   console.log("Name validation");
+
+//   //character data validation
+//   if (!isNaN(name1)) {
+//     console.log("only characters are allowed");
+//     document.getElementById("charMsg").innerHTML =
+//       "**Only characters are allowed";
+//     return false;
+//   }
+//   if (!isNaN(name2)) {
+//     document.getElementById("charMsg").innerHTML =
+//       "**Only characters are allowed";
+//     return false;
+//   }
+
+//   //check empty first name field
+//   if (name1 == "") {
+//     document.getElementById("blankMsg").innerHTML = "First name required";
+//     return false;
+//   }
+//   if (name2 == "") {
+//     document.getElementById("blankMsg").innerHTML = "Last name required";
+//     return false;
+//   }
+// }
 
 // document.getElementById("myForm").reset();
